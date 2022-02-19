3275420af3fb41aef3112b477a0df38f31443e98
Sandile-Repos <sandile.developer@gmail.com>
2022-02-19 21:58:10 +0200

    Gesture Handler 2

--- App.tsx
+++ App.tsx
@@ -1,39 +1,106 @@
-import React from "react";
-import { StyleSheet } from "react-native";
+import { Dimensions, StyleSheet, View } from "react-native";
+import {
+  Gesture,
+  GestureDetector,
+  GestureHandlerRootView,
+} from "react-native-gesture-handler";
 import Animated, {
-  useAnimatedScrollHandler,
+  useAnimatedGestureHandler,
+  useAnimatedStyle,
+  useDerivedValue,
   useSharedValue,
+  withSpring,
 } from "react-native-reanimated";
-import { Page } from "./components/Page";
 
-const WORDS = ["What's", "up", "mobile", "devs?"];
+interface AnimatedPosition {
+  x: Animated.SharedValue<number>;
+  y: Animated.SharedValue<number>;
+}
 
+const useFollowAnimatedPosition = ({ x, y }: AnimatedPosition) => {
+  const followX = useDerivedValue(() => {
+    return withSpring(x.value);
+  });
+
+  const followY = useDerivedValue(() => {
+    return withSpring(y.value);
+  });
+
+  const rStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: followX.value }, { translateY: followY.value }],
+    };
+  });
+
+  return { followX, followY, rStyle };
+};
+
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
+
+const SIZE = 80;
 export default function App() {
   const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0);
 
-  const scrollHandler = useAnimatedScrollHandler((event) => {
-    translateX.value = event.contentOffset.x;
+  const context = useSharedValue({ x: 0, y: 0 });
+  // useAnimatedGestureHandler({})
+  const gesture = Gesture.Pan()
+    .onStart(() => {
+      context.value = { x: translateX.value, y: translateY.value };
+    })
+    .onUpdate((event) => {
+      translateX.value = event.translationX + context.value.x;
+      translateY.value = event.translationY + context.value.y;
+    })
+    .onEnd(() => {
+      if (translateX.value > SCREEN_WIDTH / 2) {
+        translateX.value = SCREEN_WIDTH - SIZE;
+      } else {
+        translateX.value = 0;
+      }
+    });
+
+  const {
+    followX: blueFollowX,
+    followY: blueFollowY,
+    rStyle: rBlueCircleStyle,
+  } = useFollowAnimatedPosition({
+    x: translateX,
+    y: translateY,
+  });
+
+  const {
+    followX: redFollowX,
+    followY: redFollowY,
+    rStyle: rRedCircleStyle,
+  } = useFollowAnimatedPosition({
+    x: blueFollowX,
+    y: blueFollowY,
+  });
+
+  const { rStyle: rGreenCircleStyle } = useFollowAnimatedPosition({
+    x: redFollowX,
+    y: redFollowY,
   });
 
   return (
-    <Animated.ScrollView
-      onScroll={scrollHandler}
-      pagingEnabled
-      scrollEventThrottle={16}
-      horizontal
-      style={styles.container}
-    >
-      {WORDS.map((title, index) => {
-        return (
-          <Page
-            key={index.toString()}
-            title={title}
-            translateX={translateX}
-            index={index}
-          />
-        );
-      })}
-    </Animated.ScrollView>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <Animated.View
+          style={[
+            styles.circle,
+            { backgroundColor: "green" },
+            rGreenCircleStyle,
+          ]}
+        />
+        <Animated.View
+          style={[styles.circle, { backgroundColor: "red" }, rRedCircleStyle]}
+        />
+        <GestureDetector gesture={gesture}>
+          <Animated.View style={[styles.circle, rBlueCircleStyle]} />
+        </GestureDetector>
+      </View>
+    </GestureHandlerRootView>
   );
 }
 
@@ -41,5 +108,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+  },
+  circle: {
+    position: "absolute",
+    height: SIZE,
+    aspectRatio: 1,
+    backgroundColor: "blue",
+    borderRadius: SIZE / 2,
+    opacity: 0.8,
   },
 });
