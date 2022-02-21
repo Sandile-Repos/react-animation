00be6cb1784291c76b942b1288b34c13a9ed95cf
Sandile-Repos <sandile.developer@gmail.com>
2022-02-21 08:38:34 +0200

    ColorPicker in react-native-reanimated

--- App.tsx
+++ App.tsx
@@ -1,74 +1,81 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import {
-  GestureHandlerRootView,
-  PanGestureHandler,
-  PanGestureHandlerGestureEvent,
-} from "react-native-gesture-handler";
+import React, { useCallback } from "react";
+import { Dimensions, StyleSheet, Text, View } from "react-native";
 import Animated, {
-  cancelAnimation,
-  useAnimatedGestureHandler,
-  useDerivedValue,
+  useAnimatedStyle,
   useSharedValue,
-  withDecay,
 } from "react-native-reanimated";
-import Page, { PAGE_WIDTH } from "./components/Page";
+import { ColorPicker } from "./components/ColorPicker";
 
-const titles = ["What's", "up", "mobile", "devs?"];
+const COLORS = [
+  "red",
+  "purple",
+  "blue",
+  "cyan",
+  "green",
+  "yellow",
+  "orange",
+  "black",
+  "white",
+];
 
-type ContextType = {
-  x: number;
-};
+const BACKGROUND_COLOR = "rgba(0,0,0,0.9)";
 
-const MAX_TRANSLATE_X = -PAGE_WIDTH * (titles.length - 1);
+const { width } = Dimensions.get("window");
+
+const CIRCLE_SIZE = width * 0.8;
+const PICKER_WIDTH = width * 0.9;
 
 export default function App() {
-  const translateX = useSharedValue(0);
+  const pickedColor = useSharedValue<string | number>(COLORS[0]);
 
-  const clampedTranslateX = useDerivedValue(() => {
-    return Math.max(Math.min(translateX.value, 0), MAX_TRANSLATE_X);
-  });
+  const onColorChanged = useCallback((color: string | number) => {
+    "worklet";
+    pickedColor.value = color;
+  }, []);
 
-  const panGestureEvent = useAnimatedGestureHandler<
-    PanGestureHandlerGestureEvent,
-    ContextType
-  >({
-    onStart: (_, context) => {
-      context.x = clampedTranslateX.value;
-      cancelAnimation(translateX);
-    },
-    onActive: (event, context) => {
-      translateX.value = event.translationX + context.x;
-    },
-    onEnd: (event) => {
-      translateX.value = withDecay({ velocity: event.velocityX });
-    },
+  const rStyle = useAnimatedStyle(() => {
+    return {
+      backgroundColor: pickedColor.value,
+    };
   });
 
   return (
-    <GestureHandlerRootView style={styles.container}>
-      <PanGestureHandler onGestureEvent={panGestureEvent}>
-        <Animated.View style={{ flex: 1, flexDirection: "row" }}>
-          {titles.map((title, index) => {
-            return (
-              <Page
-                key={index.toString()}
-                translateX={clampedTranslateX}
-                index={index}
-                title={title}
-              />
-            );
-          })}
-        </Animated.View>
-      </PanGestureHandler>
-    </GestureHandlerRootView>
+    <>
+      <View style={styles.topContainer}>
+        <Animated.View style={[styles.circle, rStyle]} />
+      </View>
+      <View style={styles.bottomContainer}>
+        <ColorPicker
+          colors={COLORS}
+          start={{ x: 0, y: 0 }}
+          end={{ x: 1, y: 0 }}
+          style={styles.gradient}
+          maxWidth={PICKER_WIDTH}
+          onColorChanged={onColorChanged}
+        />
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
+  topContainer: {
+    flex: 3,
+    backgroundColor: BACKGROUND_COLOR,
+    alignItems: "center",
+    justifyContent: "center",
   },
+  bottomContainer: {
+    flex: 1,
+    backgroundColor: BACKGROUND_COLOR,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  circle: {
+    width: CIRCLE_SIZE,
+    height: CIRCLE_SIZE,
+    borderRadius: CIRCLE_SIZE / 2,
+  },
+  gradient: { height: 40, width: PICKER_WIDTH, borderRadius: 20 },
 });
