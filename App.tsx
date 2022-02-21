635862384680026bca27c6d7781269737fa6c734
Sandile-Repos <sandile.developer@gmail.com>
2022-02-21 16:58:00 +0200

    ripple effect reanimated

--- App.tsx
+++ App.tsx
@@ -1,71 +1,41 @@
-import { StatusBar } from "expo-status-bar";
-import React, { useCallback, useRef, useState } from "react";
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
-import { ScrollView } from "react-native-gesture-handler";
-import ListItem from "./components/ListItem";
-
-const TITLES = [
-  "Record the dismissible tutorial 🎥",
-  "Leave 👍🏼 to the video",
-  "Check YouTube comments",
-  "Subscribe to the channel 🚀",
-  "Leave a ⭐️ on the GitHub Repo",
-];
-
-interface TaskInterface {
-  title: string;
-  index: number;
-}
-
-const TASKS: TaskInterface[] = TITLES.map((title, index) => ({ title, index }));
-
-// const TASKS = [
-//   {
-//     index: 0,
-//     title: 'Record the dismissible tutorial 🎥',
-//   },
-//   { ... }, { ... }, { ... }
-// ];
-
-const BACKGROUND_COLOR = "#FAFBFF";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Ripple from "./components/Ripple";
 
 export default function App() {
-  const [tasks, setTasks] = useState(TASKS);
-
-  const onDismiss = useCallback((task: TaskInterface) => {
-    setTasks((tasks) => tasks.filter((item) => item.index !== task.index));
-  }, []);
-
-  const scrollRef = useRef(null);
-
   return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar style="auto" />
-      <Text style={styles.title}>Tasks</Text>
-      <ScrollView ref={scrollRef} style={{ flex: 1 }}>
-        {tasks.map((task) => (
-          <ListItem
-            simultaneousHandlers={scrollRef}
-            key={task.index}
-            task={task}
-            onDismiss={onDismiss}
-          />
-        ))}
-      </ScrollView>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Ripple
+        style={styles.ripple}
+        onTap={() => {
+          console.log("tap");
+        }}
+      >
+        <Text style={{ fontSize: 25 }}>Tap</Text>
+      </Ripple>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: BACKGROUND_COLOR,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  title: {
-    fontSize: 60,
-    marginVertical: 20,
-    paddingLeft: "5%",
+  ripple: {
+    width: 200,
+    height: 200,
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 25,
+    // iOS
+    shadowOpacity: 0.2,
+    shadowOffset: { width: 0, height: 0 },
+    shadowRadius: 20,
+    // Android
+    elevation: 2,
   },
 });
-
-export { TaskInterface };
