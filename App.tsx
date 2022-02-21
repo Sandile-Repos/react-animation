9bbf38d31a2f2fca1d2c36ad7df1b52bf13ded1c
Sandile-Repos <sandile.developer@gmail.com>
2022-02-21 11:50:20 +0200

    swipe to delete in react animated

--- App.tsx
+++ App.tsx
@@ -1,69 +1,58 @@
-import React, { useEffect } from "react";
-import { useCallback } from "react";
-import { Dimensions, TouchableOpacity } from "react-native";
-import { StyleSheet, Text, View } from "react-native";
-import Animated, {
-  useSharedValue,
-  withTiming,
-  useAnimatedProps,
-} from "react-native-reanimated";
-import { useDerivedValue } from "react-native-reanimated";
-import { ReText } from "react-native-redash";
+import { StatusBar } from "expo-status-bar";
+import React, { useCallback, useRef, useState } from "react";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+import ListItem from "./components/ListItem";
 
-import Svg, { Circle } from "react-native-svg";
+const TITLES = [
+  "Record the dismissible tutorial 🎥",
+  "Leave 👍🏼 to the video",
+  "Check YouTube comments",
+  "Subscribe to the channel 🚀",
+  "Leave a ⭐️ on the GitHub Repo",
+];
 
-const BACKGROUND_COLOR = "#444B6F";
-const BACKGROUND_STROKE_COLOR = "#303858";
-const STROKE_COLOR = "#A6E1FA";
+interface TaskInterface {
+  title: string;
+  index: number;
+}
 
-const { width, height } = Dimensions.get("window");
+const TASKS: TaskInterface[] = TITLES.map((title, index) => ({ title, index }));
 
-const CIRCLE_LENGTH = 1000; // 2PI*R
-const R = CIRCLE_LENGTH / (2 * Math.PI);
+// const TASKS = [
+//   {
+//     index: 0,
+//     title: 'Record the dismissible tutorial 🎥',
+//   },
+//   { ... }, { ... }, { ... }
+// ];
 
-const AnimatedCircle = Animated.createAnimatedComponent(Circle);
+const BACKGROUND_COLOR = "#FAFBFF";
 
 export default function App() {
-  const progress = useSharedValue(0);
+  const [tasks, setTasks] = useState(TASKS);
 
-  const animatedProps = useAnimatedProps(() => ({
-    strokeDashoffset: CIRCLE_LENGTH * (1 - progress.value),
-  }));
-
-  const progressText = useDerivedValue(() => {
-    return `${Math.floor(progress.value * 100)}`;
-  });
-
-  const onPress = useCallback(() => {
-    progress.value = withTiming(progress.value > 0 ? 0 : 1, { duration: 2000 });
+  const onDismiss = useCallback((task: TaskInterface) => {
+    setTasks((tasks) => tasks.filter((item) => item.index !== task.index));
   }, []);
 
+  const scrollRef = useRef(null);
+
   return (
-    <View style={styles.container}>
-      <ReText style={styles.progressText} text={progressText} />
-      <Svg style={{ position: "absolute" }}>
-        <Circle
-          cx={width / 2}
-          cy={height / 2}
-          r={R}
-          stroke={BACKGROUND_STROKE_COLOR}
-          strokeWidth={30}
-        />
-        <AnimatedCircle
-          cx={width / 2}
-          cy={height / 2}
-          r={R}
-          stroke={STROKE_COLOR}
-          strokeWidth={15}
-          strokeDasharray={CIRCLE_LENGTH}
-          animatedProps={animatedProps}
-          strokeLinecap={"round"}
-        />
-      </Svg>
-      <TouchableOpacity onPress={onPress} style={styles.button}>
-        <Text style={styles.buttonText}>Run</Text>
-      </TouchableOpacity>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar style="auto" />
+      <Text style={styles.title}>Tasks</Text>
+      <ScrollView ref={scrollRef} style={{ flex: 1 }}>
+        {tasks.map((task) => (
+          <ListItem
+            simultaneousHandlers={scrollRef}
+            key={task.index}
+            task={task}
+            onDismiss={onDismiss}
+          />
+        ))}
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
@@ -71,28 +60,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: BACKGROUND_COLOR,
-    alignItems: "center",
-    justifyContent: "center",
   },
-  progressText: {
-    fontSize: 80,
-    color: "rgba(256,256,256,0.7)",
-    width: 200,
-    textAlign: "center",
-  },
-  button: {
-    position: "absolute",
-    bottom: 80,
-    width: width * 0.7,
-    height: 60,
-    backgroundColor: BACKGROUND_STROKE_COLOR,
-    borderRadius: 25,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  buttonText: {
-    fontSize: 25,
-    color: "white",
-    letterSpacing: 2.0,
+  title: {
+    fontSize: 60,
+    marginVertical: 20,
+    paddingLeft: "5%",
   },
 });
+
+export { TaskInterface };
