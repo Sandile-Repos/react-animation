b4d91e8529239aba14b15e4a2c79d3a4d26effa4
Sandile-Repos <sandile.developer@gmail.com>
2022-02-21 22:09:28 +0200

    Clock loader Reanimated

--- App.tsx
+++ App.tsx
@@ -1,22 +1,42 @@
 import { StatusBar } from "expo-status-bar";
+import React, { useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
-import SlidingCounter from "./components/SlidingCounter";
+import {
+  Easing,
+  useSharedValue,
+  withRepeat,
+  withTiming,
+} from "react-native-reanimated";
+import Square from "./components/Square";
+import { N } from "./constants";
 
 export default function App() {
+  const progress = useSharedValue(0);
+
+  useEffect(() => {
+    progress.value = withRepeat(
+      withTiming(4 * Math.PI, {
+        duration: 8000,
+        easing: Easing.linear,
+      }),
+      -1
+    );
+  }, []);
+
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <View style={styles.container}>
-        <SlidingCounter />
-      </View>
-    </GestureHandlerRootView>
+    <View style={styles.container}>
+      <StatusBar style="inverted" />
+      {new Array(N).fill(0).map((_, index) => {
+        return <Square key={index} progress={progress} index={index} />;
+      })}
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#111",
     alignItems: "center",
     justifyContent: "center",
   },
